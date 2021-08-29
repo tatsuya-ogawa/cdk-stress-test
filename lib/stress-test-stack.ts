2cb1aa2e9613f331cbf7bbd3fc02d4e44d6ed552
tatsuya-ogawa <togawa83@gmail.com>
2021-08-29 23:25:39 +0900

    first commit

--- lib/stress-test-stack.ts
+++ lib/stress-test-stack.ts
@@ -1,9 +1,34 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
-export class CdkStressTestStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+interface StressTestStackProps extends cdk.StackProps{
+
+}
+
+export class StressTestStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: StressTestStackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'TheVPC', {
+      cidr: "10.0.0.0/16",
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE,
+        },
+        {
+          cidrMask: 28,
+          name: 'isolated',
+          subnetType: ec2.SubnetType.ISOLATED,
+        }
+      ]
+    })
   }
 }
