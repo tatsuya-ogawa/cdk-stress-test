4b26b2d6e9ebaaf0cebf95806e7d9ddf473b2c79
tatsuya-ogawa <togawa83@gmail.com>
2021-08-29 23:24:59 +0900

    first commit

--- bin/stress-test.ts
+++ bin/stress-test.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkStressTestStack } from '../lib/cdk-stress-test-stack';
+import { StressTestStack } from '../lib/stress-test-stack';
 
 const app = new cdk.App();
-new CdkStressTestStack(app, 'CdkStressTestStack', {
+new StressTestStack(app, 'CdkStressTestStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
